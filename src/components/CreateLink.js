51e63caec59a0a0e3f1a11e615891f1cb6fc88d8
Bianca Jemsten <biancajemsten@gmail.com>
2018-08-29 09:10:53 +0100

    added routing

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -39,7 +39,11 @@ class CreateLink extends Component {
             placeholder="The URL for the link"
           />
         </div>
-        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+        <Mutation
+          mutation={POST_MUTATION}
+          variables={{ description, url }}
+          onCompleted={() => this.props.history.push('/')}
+        >
           {postMutation => <button onClick={postMutation}>Submit</button>}
         </Mutation>
       </div>
@@ -47,4 +51,4 @@ class CreateLink extends Component {
   }
 }
 
-export default CreateLink
+export default CreateLink;
