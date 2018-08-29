d3b9c07880d6fb2cda58fee72716f26db73f4d68
Bianca Jemsten <biancajemsten@gmail.com>
2018-08-29 16:01:29 +0100

    instantiating a WebSocketLink that knows the subscriptions endpoint

--- src/index.js
+++ src/index.js
@@ -10,6 +10,34 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
 import { setContext } from 'apollo-link-context';
 import { AUTH_TOKEN } from './constants';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000',
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
