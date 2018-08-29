5cc5b551d12c46573939bad940c3579d529ad79e
Bianca Jemsten <biancajemsten@gmail.com>
2018-08-29 15:54:17 +0100

    created search page and linked in nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,11 +13,15 @@ class Header extends Component {
           <Link to="/" className="ml1 no-underline black">
           new
           </Link>
+          <div className="ml1">|</div>
+          <Link to="/search" className="ml1 no-underline black">
+        search
+          </Link>
           {authToken && (
             <div className="flex">
               <div className="ml1">|</div>
               <Link to="/create" className="ml1 no-underline black">
-            submit
+          submit
               </Link>
             </div>
           )}
